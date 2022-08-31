3868571f96d236949adca92a2ae3d13054a39450
Iury Vasconcelos <iury0393@gmail.com>
2022-08-31 09:15:44 -0300

    Criando as telas do app

--- Notey/Controller/CategoryViewController.swift
+++ Notey/Controller/CategoryViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CategoryViewController: UIViewController {
+class CategoryViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Notey/Controller/TextViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TextViewController.swift
+//  Notey
+//
+//  Created by Iury Vasconcelos on 31/08/22.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
